4da2fe8e5ae0246597f402be4d23589662d4f618
Josue Rivera <jm.riveral@uniandes.edu.co>
2021-04-18 22:02:56 -0500

    taller 1 fin

---
+++ dataCourses.ts
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Ingeniería de Sw", "Pablo Picasso", 4),
+  new Course("Fisica 2", "Kelvar Neelima", 2),
+  new Course("Dalgo", "Rodrigo Cardozo", 2),
+  new Course("Calc.Vectorial", "Sergio Fernandez", 1),
+  new Course("Futbol 2", "James R", 6)
+]
---
+++ scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Ingeniería de Sw", "Pablo Picasso", 4),
+    new Course("Fisica 2", "Kelvar Neelima", 2),
+    new Course("Dalgo", "Rodrigo Cardozo", 2),
+    new Course("Calc.Vectorial", "Sergio Fernandez", 1),
+    new Course("Futbol 2", "James R", 6)
+];
